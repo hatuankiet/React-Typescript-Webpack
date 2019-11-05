09c0e33af76e2eb34003c8ea1676a40760bc84f2
kiethao112@yahoo.com.vn <kiethao112@yahoo.com.vn>
2019-11-05 09:31:15 +0700

    sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,8 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./component/App";
+import "./styles/style.scss";
+
+const ROOT = document.querySelector(".container");
+
+ReactDOM.render(<App name="hello word" ></App>, ROOT); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,41 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const extractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry : './src/index.tsx',
+    entry: './src/index.tsx',
     output: {
-        path: path.resolve(__dirname,"build"),
-        filename : "bundle.js"
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
     },
-    module : {},
-    plugins: []
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.(sc|sa|c)ss$/,
+                use : [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./index.html",   
+        }),
+    ],
+    devtool: "source-map",
+    resolve: {
+        extensions: [".js", ".tsx", ".ts",".json"]
+    }
 }
